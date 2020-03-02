b49023823684f199bc6a7e1f913918e6f9244e1d
Ziang Lu <ziangl@alumni.cmu.edu>
2020-03-02 14:41:32 +0800

    Remove redundant files

--- components/drawer-navigator/DrawerNavigatorApp.tsx
+++
@@ -1,6 +0,0 @@
-import React, { Component } from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-
-export default class DrawerNavigatorApp extends Component {
-  public render() {}
-}
